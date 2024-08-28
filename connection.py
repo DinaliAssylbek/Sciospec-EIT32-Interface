dbd96da25f14475992bb08585ddb0e2fe211c1d0
PrinceDinali <dinali.assylbek.2003@gmail.com>
2024-08-27 18:11:32 -0600

    Before Adding 2 Graph feature

--- connection.py
+++ connection.py
@@ -1,23 +1,28 @@
+import time
+import os
+import traceback
 import serial
 import struct
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import shared_functions as sf
+
 from typing import Union, List
 from multiprocessing import Process, Queue, Event
 from queue import Empty
 from measurment_setup import set_measurement_setup, get_measurement_setup
 from output_configs import set_output_config, get_output_config
 from datetime import datetime
-import os
-import traceback
+
 
 params = {
-  "Burst Count" : 2,
-  "Frame Rate" : 1,
+  "Burst Count" : 1000,
+  "Frame Rate" : 50,
   "Excitation Frequencies" : {"Fmin" : 50000, "Fmax": 50000, "Ftype": 0},
   "Excitation Amplitude" : 0.001,
   "Excitation Switch Type" : 2,
-  "Excitation Sequence" : [(1,17), (2,18)],
-  "ADC Range" : 1,
+  "Excitation Sequence" : [(1,3), (2,4), (3,5), (4,6), (5,7), (6,8), (7,1), (8,2)],
+  "ADC Range" : 2,
   "Single Ended": {"Mode": 1, "Boundary": 1}
 }
 
@@ -27,78 +32,10 @@ configurations = {
   "Timestamp" : 1
 }
 
-def SystemMessageCallback(
-    serial, prnt_msg: bool = True, ret_hex_int: Union[None, str] = None
-):
-    """
-    Reads the message buffer of a serial connection. Also prints out the general system message.
+sequence_display = {
+  "Excitation Sequence" : [(1,3), (4,6)]
+}
 
-    Parameters
-    ----------
-    serial :
-        serial connection
-    prnt_msg : bool
-        if true print message, if false not
-    ret_hex_int : Union[None, str]
-        use ['none','hex', 'int', 'both'] to return nothing, hex or integer data or both.
-
-    Returns
-    -------
-    [None, received_hex, received, (received, received_hex)]
-        return depens on the ret_hex_int variable
-    """
-    msg_dict = {
-        "0x01": "No message inside the message buffer",
-        "0x02": "Timeout: Communication-timeout (less data than expected)",
-        "0x04": "Wake-Up Message: System boot ready",
-        "0x11": "TCP-Socket: Valid TCP client-socket connection",
-        "0x81": "Not-Acknowledge: Command has not been executed",
-        "0x82": "Not-Acknowledge: Command could not be recognized",
-        "0x83": "Command-Acknowledge: Command has been executed successfully",
-        "0x84": "System-Ready Message: System is operational and ready to receive data",
-        "0x92": "Data holdup: Measurement data could not be sent via the master interface",
-    }
-    timeout_count = 0
-    received = []
-    received_hex = []
-    data_count = 0
-
-    while True:
-        buffer = serial.read()
-        if buffer:
-            received.extend(buffer)
-            # print(received[-1])
-            # print(int.from_bytes(buffer, byteorder='big'))
-            data_count += len(buffer)
-            timeout_count = 0
-            continue
-        timeout_count += 1
-        if timeout_count >= 1:
-            # Break if we haven't received any data
-            break
-
-        received = "".join(str(received))  # If you need all the data
-    received_hex = [hex(receive) for receive in received]
-    try:
-        msg_idx = received_hex.index("0x18")
-        if prnt_msg:
-            print(msg_dict[received_hex[msg_idx + 2]])
-    except BaseException:
-        if prnt_msg:
-            print(msg_dict["0x01"])
-        prnt_msg = False
-    if prnt_msg:
-        print("message buffer:\n", received_hex)
-        print("message length:\t", data_count)
-
-    if ret_hex_int is None:
-        return
-    elif ret_hex_int == "hex":
-        return received_hex
-    elif ret_hex_int == "int":
-        return received
-    elif ret_hex_int == "both":
-        return received, received_hex
     
 def read_from_serial(q, stop_event, ser):
   while True:
@@ -109,7 +46,7 @@ def read_from_serial(q, stop_event, ser):
     stop_event.set()
     break
 
-def process_data(q, stop_event, configs, dataset_name, directory_path):
+def process_data(q, stop_event, configs, dataset_name, directory_path, live_data_queue, live_timestamp_queue, sequence):
   items = []
   frames = []
   current_frame = []
@@ -118,6 +55,7 @@ def process_data(q, stop_event, configs, dataset_name, directory_path):
   frame_count = 0
   file_output = ['18'],
   the_first = [],
+  curr_sequence = -1
   file_num = 0
   while not stop_event.is_set() or not q.empty():
     try:
@@ -126,55 +64,64 @@ def process_data(q, stop_event, configs, dataset_name, directory_path):
       items.append(num)
        # Check for the start of a frame
       if num == 180 and not frame_started:
-          frame_started = True
-          current_frame = [num]
-          frame_length = 0
-          continue
+        frame_started = True
+        current_frame = [num]
+        frame_length = 0
+        continue
       
       # If a frame has started, add to the current frame
       if frame_started:
           current_frame.append(num)
           if len(current_frame) == 2:  # second byte indicates the frame length
-              frame_length = num + 1
+            frame_length = num + 1
 
           # Check for the end of the frame
           if len(current_frame) > 2 and len(current_frame) == frame_length + 2:
-              frames.append(current_frame)
-              frame_count += 1
-              if frame_count == 1:
-                 the_first = [str(current_frame[3]), str(current_frame[4])]
-              if [str(current_frame[3]), str(current_frame[4])] == the_first:
-                if current_frame[2] == 1:
-                  if frame_count != 1:
-                    file_num += 1
-                    file_path = os.path.join(directory_path, f'{dataset_name}_{file_num}.txt')
-                    with open(file_path, 'w') as file:
-                      for line in file_output:
-                          file.write(line + '\n')
-                  now = datetime.now()
-                  file_output = ['18', str(frame_count), dataset_name, now.strftime("%Y.%m.%d. %H:%M:%S.%f")[:-3]] + configs + [str(current_frame[4]) + " " + str(current_frame[3])]
-                else:
-                  all_data = frames[-1][9:-1] + current_frame[9:-1]
-                  floats = []
-                  for i in range(0, len(all_data), 4):
-                      byte_chunk = all_data[i:i+4]
-                      if len(byte_chunk) == 4:
-                          float_number = struct.unpack('>f', bytes(byte_chunk))[0]
-                          floats.append(float_number)
-                  file_output += [' '.join(map(str, floats))]
+            frames.append(current_frame)
+            frame_count += 1
+            if frame_count == 1:
+              the_first = [str(current_frame[3]), str(current_frame[4])]
+
+            if [str(current_frame[3]), str(current_frame[4])] == the_first:
+              if current_frame[2] == 1:
+                if frame_count != 1:
+                  file_num += 1
+                  file_path = os.path.join(directory_path, f'{dataset_name}_{file_num}.txt')
+                  with open(file_path, 'w') as file:
+                    for line in file_output:
+                      file.write(line + '\n')
+                now = datetime.now()
+                file_output = ['18', str(frame_count), dataset_name, now.strftime("%Y.%m.%d. %H:%M:%S.%f")[:-3]] + configs + [str(current_frame[4]) + " " + str(current_frame[3])]
               else:
-                if current_frame[2] == 1:
-                  file_output += [str(current_frame[4]) + " " + str(current_frame[3])]
-                else:
-                  all_data = frames[-1][9:-1] + current_frame[9:-1]
-                  floats = []
-                  for i in range(0, len(all_data), 4):
-                    byte_chunk = all_data[i:i+4]
-                    if len(byte_chunk) == 4:
-                      float_number = struct.unpack('>f', bytes(byte_chunk))[0]
-                      floats.append(float_number)
-                  file_output += [' '.join(map(str, floats))]
-              frame_started = False
+                all_data = frames[-1][9:-1] + current_frame[9:-1]
+                floats = []
+                for i in range(0, len(all_data), 4):
+                  byte_chunk = all_data[i:i+4]
+                  if len(byte_chunk) == 4:
+                    float_number = struct.unpack('>f', bytes(byte_chunk))[0]
+                    floats.append(float_number)
+                curr_sequence = (curr_sequence + 1) % len(sequence)
+                for index in range(len(live_data_queue[0])):
+                  live_data_queue[0][index].put(floats[index * 2])
+                live_timestamp_queue[0].put(time.time())
+                file_output += [' '.join(map(str, floats))]
+            else:
+              if current_frame[2] == 1:
+                file_output += [str(current_frame[4]) + " " + str(current_frame[3])]
+              else:
+                all_data = frames[-1][9:-1] + current_frame[9:-1]
+                floats = []
+                for i in range(0, len(all_data), 4):
+                  byte_chunk = all_data[i:i+4]
+                  if len(byte_chunk) == 4:
+                    float_number = struct.unpack('>f', bytes(byte_chunk))[0]
+                    floats.append(float_number)
+                curr_sequence = (curr_sequence + 1) % len(sequence)
+                for index in range(len(live_data_queue[0])):
+                  live_data_queue[curr_sequence][index].put(floats[index * 2])
+                live_timestamp_queue[curr_sequence].put(time.time())
+                file_output += [' '.join(map(str, floats))]
+            frame_started = False
     except Empty:
       continue
     except Exception as e:
@@ -182,13 +129,11 @@ def process_data(q, stop_event, configs, dataset_name, directory_path):
       print(f"An error occurred: {e}")
       print("Traceback details:")
       print(tb)
-      # print(f"Error converting item {item}: {e}")]
   file_num += 1
   file_path = os.path.join(directory_path, f'{dataset_name}_{file_num}.txt')
   with open(file_path, 'w') as file:
     for line in file_output:
         file.write(line + '\n')
-  # print(file_output)
     
 def StartStopMeasurement(q, stop_event) -> list:
     ser = sf.init_serial()
@@ -199,33 +144,73 @@ def StartStopMeasurement(q, stop_event) -> list:
     ser.write(bytearray([0xB4, 0x01, 0x00, 0xB4]))
     return measurement_data_hex
 
-def measurment(dataset_name, directory_path):
-  configs = get_measurement_setup()
+def measurment(dataset_name, directory_path, live_data_queue, live_timestamp_queue, configs):
+  # configs = get_measurement_setup()
+  # for i in range(len(configs[-1])):
+  #   live_data_queue.append([Queue() for j in range(8)])
+  #   live_timestamp_queue.append(Queue())
   q = Queue()
   stop_event = Event()
   read_process = Process(target=StartStopMeasurement, args=(q, stop_event))
-  process_process = Process(target=process_data, args=(q, stop_event, configs, dataset_name, directory_path))
+  process_process = Process(target=process_data, args=(q, stop_event, configs[:-2], dataset_name, directory_path, live_data_queue, live_timestamp_queue, configs[-1]))
   read_process.start()
   process_process.start()
-# set_measurement_setup(params)
+  
+def update_graph(frame, x_data, y_data, live_data_queue, lines, live_timestamp_queue):
+    try:
+      while True:
+        for i in range(len(y_data)):
+          y_data[i].append(live_data_queue[i].get_nowait())
+        x_data.append(live_timestamp_queue.get_nowait())
+    except Empty:
+      pass
+
+    if x_data:
+      for i, line in enumerate(lines):
+        line.set_data(x_data, y_data[i])
+        ax = line.axes
+        ax.set_xlim(x_data[0], x_data[-1])
+        ax.set_ylim(min(y_data[i]), max(y_data[i]))
+    return lines
+
+def measure():
+  configs = get_measurement_setup()
+  print(configs[-1])
+  live_data_queue = [[Queue() for i in range(8)] for j in range(len(configs[-1]))]
+  live_timestamp_queue = [Queue() for i in range(len(configs[-1]))]
+  measurment('hello', 'D:/Sciospec-EIT32-Interface/test_output', live_data_queue, live_timestamp_queue, configs)
+  figs, axes, lines, anis = [], [], [], []
+  for i in range(len(configs[-1])):
+    fig, ax = plt.subplots(8,1, sharex=True)
+    figs.append(fig)
+    axes.append(ax)
+    
+    x_data, y_data = [], [[] for _ in range(8)]
+    line = [a.plot([], [])[0] for a in ax]
+    lines.append(line)
+
+    ani = animation.FuncAnimation(fig, update_graph, fargs=(x_data, y_data, live_data_queue[i], line, live_timestamp_queue[i]), interval=1000)
+    anis.append(ani)
+  plt.show()
+
 
 if __name__ == '__main__':
-   set_measurement_setup(params)
-   set_output_config(configurations)
-   measurment('hello', '//chips.eng.utah.edu/home/u1462232/.win_desktop/Sciospec-EIT32-Interface/test_output')
+  set_measurement_setup(params)
+  set_output_config(configurations)
+  measure()
+  # live_data_queue = [[Queue() for i in range(8)] for j in range(1)]
+  # live_timestamp_queue = [Queue()]
+  # measurment('hello', '//chips.eng.utah.edu/home/u1462232/.win_desktop/Sciospec-EIT32-Interface/test_output', live_data_queue, live_timestamp_queue)
+  # figs, axes, lines, anis = [], [], [], []
+  # for i in range(2):
+  #   fig, ax = plt.subplots(8,1, sharex=True)
+  #   figs.append(fig)
+  #   axes.append(ax)
+    
+  #   x_data, y_data = [], [[] for _ in range(8)]
+  #   line = [a.plot([], [])[0] for a in ax]
+  #   lines.append(line)
 
-# if __name__ == '__main__':
-#     q = Queue()
-#     stop_event = Event()
-
-#     set_measurement_setup(params)
-#     get_measurement_setup()
-#     print("--------------------------------")
-#     # set_output_config(configurations)
-#     # get_output_config()
-#     # print("--------------------------------")
-#     read_process = Process(target=StartStopMeasurement, args=(q, stop_event))
-#     process_process = Process(target=process_data, args=(q, stop_event))
-
-#     read_process.start()
-#     process_process.start()
+  #   ani = animation.FuncAnimation(fig, update_graph, fargs=(x_data, y_data, live_data_queue[i], line, live_timestamp_queue), interval=1000)
+  #   anis.append(ani)
+  # plt.show()
